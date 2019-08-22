593ab7215d232cda138682f9441ea3422f9a005d
jinalshah999 <jinalshah999@gmail.com>
2019-08-22 11:58:57 +0530

    server ready

---
+++ index.js
@@ -0,0 +1,28 @@
+let express = require('express')
+let app = express();
+
+let http = require('http');
+let server = http.Server(app);
+
+let socketIO = require('socket.io');
+let io = socketIO(server);
+
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+    socket.on('newmessage',(x)=>{
+        console.log(x);
+    });
+    
+});
+
+server.listen(port, () => {
+    console.log(`started on port: ${port}`);
+});
+
+
+// socket.on('new-message', (message) => {
+//     io.emit('new-message', message+"  server");
+//   console.log(message);
+// });
